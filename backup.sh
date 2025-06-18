5d3d4a980b931ac1e624ba25defc755e14b56fc4
Nayan Bakhadyo <ugalnayanbakhadyo@gmail.com>
2025-06-18 15:24:28 -0500

    db backup of 30 days

--- backup.sh
+++ backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Set variables
+# Set paths
+DB_FILE="/home/ubuntu/fund_management_system/db.sqlite3"
 BACKUP_DIR="/home/ubuntu/db_backups"
-DB_PATH="/home/ubuntu/fund_management_system/db.sqlite3"
-DATE=$(date +"%Y%m%d_%H%M%S")
-BACKUP_NAME="db_backup_$DATE.sqlite3"
+FILENAME="db_backup_$(date +%F_%H-%M-%S).sqlite3"
 
-# Make sure backup directory exists
-mkdir -p $BACKUP_DIR
+# Ensure the backup directory exists
+mkdir -p "$BACKUP_DIR"
 
-# Copy DB file to backup directory with timestamp
-cp $DB_PATH $BACKUP_DIR/$BACKUP_NAME
+# Copy the database file
+cp --preserve=all "$DB_FILE" "$BACKUP_DIR/$FILENAME"
+
+# Delete backups older than 30 days
+find "$BACKUP_DIR" -name "db_backup_*.sqlite3" -type f -mtime +30 -exec rm -f {} \;
